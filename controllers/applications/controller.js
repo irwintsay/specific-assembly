dc435f464a257ff27d56879825c36ad15618313e
Irwin Tsay <irwintsay@gmail.com>
2017-03-15 03:50:33 -0400

    Modify Applications controller, model, and form to get it working

--- controllers/applications/controller.js
+++ controllers/applications/controller.js
@@ -2,8 +2,10 @@ const Application       = require('../../models/application');
 let controller          = {};
 
 controller.create       = (req, res) => {
+  console.log(req.body.application);
+  console.log(req.session.user.id);
   Application
-  .create(req.body.application)
+  .create(req.body.application, req.session.user.id)
   .then(() => res.redirect('/careers/thankyou'));
 };
 
