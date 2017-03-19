21afc19f45d96063d364e011540f1993cf52f5e9
Irwin Tsay <irwintsay@gmail.com>
2017-03-19 15:01:54 -0400

    Added register link, finished styling login page

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -7,5 +7,6 @@ router.get('/login', controller.login);
 router.get('/profile', AuthService.restrict, controller.profile);
 router.post('/', controller.create);
 router.post('/login', controller.processLogin);
+// router.post('/logout', controller.processLogout);
 
 module.exports        = router;
