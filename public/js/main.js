42878d7ba02f4bbceb7ae793c3baec453d982431
Irwin Tsay <irwintsay@gmail.com>
2017-03-21 16:39:41 -0400

    Add nav bar transition effect

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Friendly welcome message
   console.log('Welcome to Specific Assembly!');
   console.log('We\'re looking for talented developers like you!!');
+  
+  // Nav and Title effect
+  const nav = document.querySelector('nav');
+  const title = document.querySelector('nav h2');
+
+  document.addEventListener('scroll', () => {
+    if(window.scrollY > 60) {
+      nav.classList.add('black-bg');
+      title.classList.remove('invisible');
+    } else {
+      nav.classList.remove('black-bg');
+      title.classList.add('invisible');
+    }
+  });
+
 });
